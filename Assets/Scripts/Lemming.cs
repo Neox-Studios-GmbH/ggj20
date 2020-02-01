ac99a22089c3708612973ee1a02dbb4bc352c3e4
Dennis_Clausen <Dennis_Clausen@NEOX-DRIVE.DE>
2020-02-01 21:39:23 +0100

    Created TechnoLizard script, added Animations and Walking/Rotation sound. Imported InputSystem and configured control scheme. Improved and extended Lemming, added Lemming Animator.

--- Assets/Scripts/Lemming.cs
+++ Assets/Scripts/Lemming.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
@@ -9,85 +8,132 @@ namespace GGJ20
     public class Lemming : FactoryItem
     {
         // --- Enums ------------------------------------------------------------------------------------------------------
+        public enum State
+        {
+            Idle = 0,
+            Run = 1,
+            Suicide = 2
+        }
 
         // --- Nested Classes ---------------------------------------------------------------------------------------------
 
         // --- Fields -----------------------------------------------------------------------------------------------------
         [Header("Suicide")]
-        [SerializeField] private float _suicideJumpStrengh = 1f;
-        [SerializeField, Range(1,5)] private int _suicidalTendency = 1;
-        private FloatRange _suicideRange = new FloatRange(0.005f, 0.02f);
-        private float _suicideRate;
+        [SerializeField] private float _suicideJumpStrength = 1f;
+        [SerializeField, Range(1, 5)] private int _suicidalTendency = 1;
+        [SerializeField] private FloatRange _suicideAngleRange = new FloatRange(30f, 80f);
+        [SerializeField] private FloatRange _suicideTorqueRange = new FloatRange(1f, 10f);
 
         [SerializeField] private float _runSpeed = 2f;
         [SerializeField] private CapsuleCollider2D _collider;
         [SerializeField] private SpriteRenderer _renderer;
         [SerializeField] private Rigidbody2D _rigidbody;
+        [SerializeField] private Animator _animator;
         [SerializeField] private LayerMask _groundingMask;
 
         [Header("Cheers")]
         [SerializeField] private float _randomCheerChance;
         [SerializeField] private float _randomCheerInterval;
 
+        private State _state = State.Idle;
+
         private int _runningDirection = 1;
-        private bool _isSuiciding = false;
-        private float _cheerCooldown;
+        private float _suicideRate;
+        private Delay _cheerCooldown;
+
+        private readonly FloatRange SUICIDE_RANGE = new FloatRange(0.005f, 0.02f);
+        private readonly int ANIM_IS_FLAILING = Animator.StringToHash("IsFlailing");
+        private readonly int ANIM_SUICIDE = Animator.StringToHash("Suicide");
+
         // --- Properties -------------------------------------------------------------------------------------------------
         public int SuicidalTendency { get => _suicidalTendency; set { _suicidalTendency = Mathf.Clamp(value, 1, 5); } }
 
         // --- Unity Functions --------------------------------------------------------------------------------------------
         private void Start()
         {
-            _cheerCooldown = _randomCheerInterval;
-            _runningDirection = UnityEngine.Random.Range(0, 2)*2 - 1;
-            _suicideRate = UnityEngine.Random.Range(_suicideRange.Min, _suicideRange.Max) * _suicidalTendency;
+            _runningDirection = Random.Range(0, 2) * 2 - 1;
+            _renderer.flipX = _runningDirection == -1;
 
+            _cheerCooldown = new Delay(_randomCheerInterval);
+            _suicideRate = SUICIDE_RANGE.GetRandom() * _suicidalTendency;
+
+            SwitchState(State.Run);
         }
+
         private void Update()
         {
-            if(_isSuiciding)
-                return;
-
-            CheckForEdges();
-            Run();
-            Cheer();
+            switch(_state)
+            {
+                case State.Idle:
+                    // TODO: Locate end of escalator and switch to Run
+                    break;
+                case State.Run:
+                    Run();
+                    CheckForEdges();
+                    Cheer();
+                    break;
+                case State.Suicide:
+                    break;
+            }
         }
 
-        // --- Public/Internal Methods ------------------------------------------------------------------------------------
+        // --- Public/Internal Methods ------------------------------------------------------------------------------------        
+
 
         // --- Protected/Private Methods ----------------------------------------------------------------------------------
+        private void SwitchState(State state)
+        {
+            _state = state;
+            switch(_state)
+            {
+                case State.Idle:
+                    _animator.SetBool(ANIM_IS_FLAILING, false);
+                    break;
+                case State.Run:
+                    _animator.SetBool(ANIM_IS_FLAILING, true);
+                    break;
+                case State.Suicide:
+                    Suicide();
+                    break;
+            }
+        }
+
         private void CheckForEdges()
         {
-            Vector3 startLeft = transform.position + new Vector3(-_collider.size.x/2 * transform.localScale.x, -_collider.size.y/2 * transform.localScale.y, 0);
-            Vector3 startRight = transform.position + new Vector3(_collider.size.x/2 * transform.localScale.x, -_collider.size.y/2 * transform.localScale.y, 0);
-
-            RaycastHit2D hitLeft = Physics2D.Linecast(startLeft, startLeft + new Vector3(0, -1, 0), _groundingMask);
-            RaycastHit2D hitRight = Physics2D.Linecast(startRight, startRight + new Vector3(0, -1, 0), _groundingMask);
-
-            //Debug.DrawLine(startLeft, startLeft + new Vector3(0, -1, 0), Color.green, 0.1f);
-            //Debug.DrawLine(startRight, startRight + new Vector3(0, -1, 0), Color.red, 0.1f);
-
-            if(hitRight.collider != null && hitLeft.collider != null) 
+            Bounds bounds = _collider.bounds;
+            Vector2 p1 = bounds.min;
+            if(_runningDirection == 1)
             {
-                return;
+                p1 += Vector2.right * bounds.size.x;
             }
 
-            if(UnityEngine.Random.Range(0f, 1f) - _suicideRate < 0)
-            {
-                Suicide();
-                return;
-            }
+            Vector2 p2 = p1 + Vector2.down;
+            RaycastHit2D groundHit = Physics2D.Linecast(p1, p2, _groundingMask);
+            Debug.DrawLine(p1, p2, Color.green, Time.deltaTime);
 
-            Flip();
+            if(groundHit.collider == null)
+            {
+                if(Random.Range(0f, 1f) < _suicideRate)
+                {
+                    SwitchState(State.Suicide);
+                }
+                else
+                {
+                    Flip();
+                }
+            }
         }
 
         private void Suicide()
         {
-            _isSuiciding = true;
+            _animator.SetTrigger(ANIM_SUICIDE);
+
             _rigidbody.constraints = RigidbodyConstraints2D.None;
-            _rigidbody.AddForceAtPosition(
-                new Vector2(_suicideJumpStrengh * _runningDirection / 2, _suicideJumpStrengh * 3),
-                (Vector2)transform.position - _collider.size/2,
+            Vector2 suicideDirection = Quaternion.Euler(0f, 0f, _suicideAngleRange.GetRandom() * _runningDirection)
+                * (Vector2.right * _runningDirection * _suicideJumpStrength);
+
+            _rigidbody.AddForce(suicideDirection, ForceMode2D.Impulse);
+            _rigidbody.AddTorque(_suicideTorqueRange.GetRandom() * _suicideJumpStrength * _runningDirection,
                 ForceMode2D.Impulse);
 
             SoundManager.PlayRandomAhh(transform.position);
@@ -96,35 +142,30 @@ namespace GGJ20
         private void Flip()
         {
             _renderer.flipX = !_renderer.flipX;
-            _runningDirection = _runningDirection == 1 ? -1 : 1;
+            _runningDirection *= -1;
         }
 
         private void Run()
         {
-            transform.position += new Vector3(Time.deltaTime * _runningDirection * _runSpeed, 0, 0);
+            transform.position += Vector3.right * _runSpeed * _runningDirection * Time.deltaTime;
         }
 
         private void Cheer()
         {
-            _cheerCooldown = Mathf.Max(0f, _cheerCooldown - Time.deltaTime);
+            if(_state == State.Suicide)
+                return;
 
-            if(_cheerCooldown == 0f)
+            if(_cheerCooldown.HasElapsed)
             {
-                _cheerCooldown = _randomCheerInterval;
+                _cheerCooldown.Reset();
 
-                if(UnityEngine.Random.Range(0f,1f) - _randomCheerChance > 0)
+                if(Random.Range(0f, 1f) < _randomCheerChance)
                 {
-                    return;
+                    SoundManager.PlayRandomCheer(transform.position);
                 }
-
-                SoundManager.PlayRandomCheer(transform.position);
             }
         }
 
-        public void ButtonClick()
-        {
-            Suicide();
-        }
         // --------------------------------------------------------------------------------------------
     }
 
